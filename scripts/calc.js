3ef1a753f23d4c7d60325eaf158a2d7f4b5ac37a
developeryago <contato.yagomachado@outlook.com>
2023-04-28 12:22:55 -0300

    bug fix

--- scripts/calc.js
+++ scripts/calc.js
@@ -2,7 +2,31 @@ var nome = document.querySelector('input#name')
 var idade = document.querySelector('input#idade')
 var peso = document.querySelector('input#peso')
 var altura = document.querySelector('input#altura')
+var resp = document.getElementById('resp')
+var id = Number(nome)
+var alt = Number(altura)
+var pes = Number(peso)
+
+function verifidade(n){
+    if (Number(n) >= 0 && Number(n) <= 100){
+        return true
+    } else{
+        return false
+    }
+}
+
+function verfpeso(p){
+    if (Number(p) <= 0 && Number(p) > 120){
+        return false
+    } else{
+        return true
+    }
+}
 
 function calc(){
-    alert('Lincado!')
+    if (verifidade(nome)){
+        alert('Tudo OK!')
+    } else{
+        alert('Deu B.O!')
+    }
 }
