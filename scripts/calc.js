9b99ec046d6fa79bcfba34cc359069eace1a69a1
developeryago <contato.yagomachado@outlook.com>
2023-04-28 09:52:35 -0300

    start the project

---
+++ scripts/calc.js
@@ -0,0 +1,8 @@
+var nome = document.querySelector('input#name')
+var idade = document.querySelector('input#idade')
+var peso = document.querySelector('input#peso')
+var altura = document.querySelector('input#altura')
+
+function calc(){
+    alert('Lincado!')
+}
